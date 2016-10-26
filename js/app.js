c73b2fd8b9e1d23a5c8d3154ddc18d431ea19b92
sveloo <sanjeev.veloo@gmail.com>
2016-10-26 16:15:59 +0800

    Initial edits

--- js/app.js
+++ js/app.js
@@ -4,10 +4,24 @@ var Model = [
 
 
 var Map = function(data) {
+
+    initMap();
+
+
 }
 
 
 var ViewModel = function() {
+
+    var map;
+
+    function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+        });
+    }
+
 }
 
 
