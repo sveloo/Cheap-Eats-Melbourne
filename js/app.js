aa08f3bc023a2c52fdd57186ef64d73d580e2925
sveloo <sanjeev.veloo@gmail.com>
2016-11-09 17:16:07 +0800

    Having a go at filtering and added mySelectedCuisine function. Got it to respond with a console.log click for starters.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//Initialise my global variables
+// Declaring global variables
 var map;
 var vm;
 var markers;
@@ -13,41 +13,36 @@ var contentString;
 var restaurantInfoWindow;
 
 
-// Restaurants data consisting of name, coords (lat & lng coordinates) and cuisine category for filtering.
-// This list taken from a recent review by TimeOut magazine of the top 20 restaurants in Melbourne with meals under $20.
-
-// Model information.
+// THE MODEL
+// An array to store all the restaurant information.
 var restaurantArray = [
     {name: 'Soi 38', coords: {lat: -37.8124841, lng: 144.9697461}, cuisine: 'Thai'},
     {name: 'ShanDong MaMa', coords: {lat: -37.8126419, lng: 144.9651844}, cuisine: 'Asian'},
-    {name: 'Very Good Falafel', coords: {lat: -37.7624218, lng: 144.9605451}, cuisine: 'Kebabs'},
-    {name: 'Biggie Smalls', coords: {lat: -37.8057634, lng: 144.9811375}, cuisine: 'Kebabs'},
-    {name: 'Naked for Satan', coords: {lat: -37.7987607, lng: 144.9760093}, cuisine: 'Spanish'},
-    {name: 'Pellegrini\'s Espresso Bar', coords: {lat: -37.8116955, lng: 144.9690115}, cuisine: 'Italian'},
+    {name: 'Pellegrini\’s', coords: {lat: -37.8116955, lng: 144.9690115}, cuisine: 'Italian'},
     {name: 'Grand Trailer Park Taverna', coords: {lat: -37.8121679, lng: 144.970828}, cuisine: 'Burgers'},
-    {name: 'Heartattack and Vine', coords: {lat: -37.7976326, lng: 144.9649691}, cuisine: 'Pub Meals'},
-    {name: 'Laksa King', coords: {lat: -37.7878707, lng: 144.9274462}, cuisine: 'Asian'},
-    {name: 'Trippy Taco', coords: {lat: -37.8065107, lng: 144.9802333}, cuisine: 'Mexican'},
     {name: 'Shujinko', coords: {lat: -37.8113177, lng: 144.9649363}, cuisine: 'Japanese'},
-    {name: 'Bimbo Deluxe', coords: {lat: -37.7960887, lng: 144.9768523}, cuisine: 'Pizza'},
-    {name: 'Arbory Bar and Eatery', coords: {lat: -37.8189329, lng: 144.9639345}, cuisine: 'Pub Meals'},
-    {name: 'Slice Girls West', coords: {lat: -37.8018787, lng: 144.9045083}, cuisine: 'Burgers'},
-    {name: 'Middle Fish', coords: {lat: -37.802564, lng: 144.9568226}, cuisine: 'Seafood'},
     {name: 'Game Chicken', coords: {lat: -37.8143223, lng: 144.9587169}, cuisine: 'Korean'},
-    {name: 'African Taste', coords: {lat: -37.8037938, lng: 144.8899948}, cuisine: 'African'},
     {name: 'Purple Peanuts Japanese Café', coords: {lat: -37.8186653, lng: 144.9520687}, cuisine: 'Japanese'},
-    {name: 'Katarina Zrinski Restaurant', coords: {lat: -37.8043028, lng: 144.9023621}, cuisine: 'Russian'},
-    {name: 'Tiba\'s Lebanese Food', coords: {lat: -37.7661628, lng: 144.9603235}, cuisine: 'Kebabs'}
+    {name: 'Pho Dzung City Noodle Shop', coords: {lat: -37.810920, lng: 144.967676}, cuisine: 'Vietnamese'}
 ];
+
+// An array to store all the markers and corresponsing Google Map API marker information.
+markers = [];
+
+// An array to store all the cuisines .
+cuisines = [];
+
+// An array to store the user cuisine selection from the dropdown list.
+selectedCuisine = [];
 
 
 // Initialise Google Maps
 function initMap() {
 
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -37.9701542, lng: 144.4927086},
+        center: {lat: -37.813531, lng: 144.965633},
         mapTypeId: 'terrain',
-        zoom: 16
+        zoom: 15
     });
 
     //Initialise Knockout
@@ -56,24 +51,10 @@ function initMap() {
 }
 
 
-// Error call handle
-function googleError() {
-    alert('For some reason Google Maps isn\'t loading, have you checked your connection?')
-}
-
-
+// Initialise the ViewModel
 var ViewModel = function() {
 
     var self = this;
-
-    // An array to store all the markers and corresponsing Google Map API marker information.
-    markers = [];
-
-    // An array to store all the cuisines .
-    cuisines = [];
-
-    // An array to store the user cuisine selection from the dropdown list.
-    selectedCuisine = [];
 
     // Setup boundaries using the Google Maps API.
     bounds = new google.maps.LatLngBounds();
@@ -85,7 +66,7 @@ var ViewModel = function() {
     self.myCuisines = ko.observableArray(cuisines);
 
     // Make the selectedCuisine array an observable array in this instance.
-    self.myCuisine = ko.observableArray(selectedCuisine)
+    self.mySelectedCuisine = ko.observableArray(selectedCuisine)
 
     // Make the restaurant array an observable array in this instance.
     self.myRestaurants = ko.observableArray(restaurantArray);
@@ -116,7 +97,7 @@ var ViewModel = function() {
 
         // Fit the map to the boundaries of the markers.
         bounds.extend(marker.position);
-        map.fitBounds(bounds);
+        // map.fitBounds(bounds);
 
         // Create an onclick event to open an infowindow at each marker.
         marker.addListener('click', function() {
@@ -185,17 +166,30 @@ var ViewModel = function() {
 
     });
 
-    self.selectCuisine = function(selectedCuisine) {
+    self.myMarkers = ko.observableArray(markers);
+
+    self.mySelectedCuisine = function(selectedCuisine) {
         console.log("You selected " + selectedCuisine);
+        // Do stuff here to show only selectedCuisine markers and restaurant list.
+        // filteredCuisines = ko.utils.arrayFilter(this.myMarkers(), function () {
+        //      return (marker.cuisine === selectedCuisine);
+        //      console.log(myMarkers);
+        //      self.myRestaurants(myMarkers);
+        // });
     }
 
     self.populateInfoWindow = function (restaurant){
         var marker = restaurant.marker;
         google.maps.event.trigger(marker, 'click');
-
     };
 
+
 // Closes the ViewModel
+}
+
+// Error call handle
+function googleError() {
+    alert('For some reason Google Maps isn\'t loading, have you checked your Internet or Wifi connection?')
 }
 
 
